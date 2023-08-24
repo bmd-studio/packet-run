91e84b3e1a731f413b82b46429b63a8fe9086b73
Lei Nelissen <lei@codified.nl>
2023-08-24 15:00:49 +0200

    fix: migration error

--- server/packages/backend/src/migrations/Migration20230824121817.ts
+++ server/packages/backend/src/migrations/Migration20230824121817.ts
@@ -5,8 +5,6 @@ export class Migration20230824121817 extends Migration {
     async up(): Promise<void> {
         this.addSql('alter table `terminal` add column `run_id` text null constraint `terminal_run_id_foreign` references `run` (`id`) on update cascade on delete set null;');
         this.addSql('create unique index `terminal_run_id_unique` on `terminal` (`run_id`);');
-
-        this.addSql('drop index `run_terminal_id_unique`;');
     }
 
 }
