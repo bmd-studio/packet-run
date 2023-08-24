1f0b0bd01ae39dd1eef7aa6c36700319e03a4727
Lei Nelissen <lei@codified.nl>
2023-08-24 15:42:14 +0200

    fix: order terminals so the slots do not interfere

--- server/packages/backend/src/seeders/TerminalSeeder.ts
+++ server/packages/backend/src/seeders/TerminalSeeder.ts
@@ -19,16 +19,7 @@ export class TerminalSeeder extends Seeder {
             em.create(Terminal, { id: 10, type: TerminalType.ROUTER }),
             em.create(Terminal, { id: 11, type: TerminalType.ROUTER }),
             em.create(Terminal, { id: 12, type: TerminalType.RECEIVER }),
-        ]
-
-        // em.create(Terminal, { id: 13, type: TerminalType.SPARE });
-        // em.create(Terminal, { id: 14, type: TerminalType.SPARE });
-        // em.create(Terminal, { id: 15, type: TerminalType.SPARE });
-        // em.create(Terminal, { id: 16, type: TerminalType.SPARE });
-        // em.create(Terminal, { id: 17, type: TerminalType.SPARE });
-        // em.create(Terminal, { id: 18, type: TerminalType.SPARE });
-        // em.create(Terminal, { id: 19, type: TerminalType.SPARE });
-        // em.create(Terminal, { id: 20, type: TerminalType.SPARE });
+        ];
 
         await em.flush();
 
@@ -38,29 +29,17 @@ export class TerminalSeeder extends Seeder {
         const server2 = terminals[9];
         const receiver = terminals[12];
 
-        // sender.connectionsTo.add([ gateway ]);
-        // gateway.connectionsTo.add([ receiver, router3 ]);
-        // router1.connectionsTo.add([  ]);
-        // router2.connectionsTo.add([ server1 ]);
-        // router3.connectionsTo.add([ gateway ]);
-        // router4.connectionsTo.add([ router2, router5, router1 ]);
-        // router5.connectionsTo.add([ router1, router2, router4 ]);
-        // router6.connectionsTo.add([ router3, router2, server2 ]);
-        // router7.connectionsTo.add([ router1, router4, router3 ]);
-        // server1.connectionsTo.add([ router2, gateway ]);
-        // server2.connectionsTo.add([ router6, gateway ]);
-
         sender.connectionsTo.add([ gateway ]);
         gateway.connectionsTo.add([ receiver, terminals[6] ]);
-        server1.connectionsTo.add([ terminals[4], gateway ]);
+        server1.connectionsTo.add([ gateway, terminals[4] ]);
         server2.connectionsTo.add([ terminals[10], gateway ]);
 
         terminals[3].connectionsTo.add([ terminals[6], terminals[8], terminals[11] ]);
         terminals[4].connectionsTo.add([ server1, terminals[7], terminals[11] ]);
-        terminals[6].connectionsTo.add([ gateway, terminals[3], terminals[10] ]);
-        terminals[7].connectionsTo.add([ terminals[4], terminals[10] ]);
-        terminals[8].connectionsTo.add([ terminals[3], terminals[11] ]);
-        terminals[10].connectionsTo.add([ server2, terminals[6], terminals[7] ]);
+        terminals[6].connectionsTo.add([ terminals[10], gateway, terminals[3] ]);
+        terminals[7].connectionsTo.add([ terminals[10], terminals[4] ]);
+        terminals[8].connectionsTo.add([ terminals[11], terminals[3] ]);
+        terminals[10].connectionsTo.add([ terminals[6], terminals[7], server2 ]);
         terminals[11].connectionsTo.add([ terminals[3], terminals[4], terminals[8] ]);
 
         await em.flush();
