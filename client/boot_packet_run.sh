63f7f1fed2f30c40903a86e9903c950fedf2ca6b
Lei Nelissen <lei@codified.nl>
2023-09-19 13:58:43 +0200

    feat: wait for server to be available before starting chrome

--- client/boot_packet_run.sh
+++ client/boot_packet_run.sh
@@ -1,12 +1,22 @@
+printf 'Booting [PACKET RUN]...\n'
+
 # Check if there are git updates to the repository
-git fetch origin || true
-git pull || true
+# git fetch origin || true
+# git pull || true
 
 # Export all config entries in packet_run_config.txt as environment variables
 export $(grep -v '^#' /boot/packet_run_config.txt | xargs)
 
 # Generate the origin
 ORIGIN="http://$PACKET_RUN_SERVER_IP:3000"
+
+# Wait for the origin to become available
+printf 'Waiting for network connection'
+until $(curl --output /dev/null --silent --head --fail $ORIGIN); do
+    printf '.'
+    sleep 5
+done
+printf 'Host found!'
 
 # Then, launch Chromium based on those variables
 chromium-browser "$ORIGIN/router/$PACKET_RUN_TERMINAL_ID" \
