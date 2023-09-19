aaa33d0076afc76b8d737e688a8d968377f0ac35
Lei Nelissen <lei@codified.nl>
2023-09-19 14:08:24 +0200

    fix: formatting for script

--- client/boot_packet_run.sh
+++ client/boot_packet_run.sh
@@ -1,5 +1,11 @@
-printf 'Booting [PACKET RUN]...\n'
+printf '  ____   _    ____ _  _______ _____   ____  _   _ _   _ \n'
+printf ' |  _ \ / \  / ___| |/ / ____|_   _| |  _ \| | | | \ | |\n'
+printf ' | |_) / _ \| |   | '\'' /|  _|   | |   | |_) | | | |  \| |\n'
+printf ' |  __/ ___ \ |___| . \| |___  | |   |  _ <| |_| | |\  |\n'
+printf ' |_| /_/   \_\____|_|\_\_____| |_|   |_| \_\\___/|_| \_|\n\n'
 
+printf 'Booting...\n'
+                                                        
 # Check if there are git updates to the repository
 # git fetch origin || true
 # git pull || true
@@ -16,11 +22,11 @@ until $(curl --output /dev/null --silent --head --fail $ORIGIN); do
     printf '.'
     sleep 5
 done
-printf 'Host found!'
+printf '\nHost was found and available! Launching Packet Run...\n'
 
 # Then, launch Chromium based on those variables
 chromium-browser "$ORIGIN/router/$PACKET_RUN_TERMINAL_ID" \
     --start-fullscreen \
     --unsafely-treat-insecure-origin-as-secure=$ORIGIN \
     --hide-crash-restore-bubble \
-    --test-type
+    --test-type > /dev/null
