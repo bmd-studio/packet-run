5275b9059d3bc8656656a9e65c5b7b74a765c5a3
Lei Nelissen <lei@codified.nl>
2023-10-03 15:21:22 +0200

    fix: migration file

--- server/packages/backend/src/migrations/Migration20230922100109.ts
+++ server/packages/backend/src/migrations/Migration20230922100109.ts
@@ -2,8 +2,8 @@ import { Migration } from '@mikro-orm/migrations';
 
 export class Migration20230922100109 extends Migration {
 
-  async up(): Promise<void> {
-    this.addSql('alter table `address` add column `is_internal_ip` integer not null default false;');
-  }
+    async up(): Promise<void> {
+        this.addSql('alter table `address` add column `is_internal_ip` integer not null default false;');
+    }
 
 }
