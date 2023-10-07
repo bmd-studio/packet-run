950a472c3abdf166307ca0b2b2f607bcc6b2d2f7
Lei Nelissen <lei@codified.nl>
2023-10-07 20:14:54 +0200

    feat: add text border around titles and subtitles

--- server/packages/frontend/src/components/Typography/index.tsx
+++ server/packages/frontend/src/components/Typography/index.tsx
@@ -3,11 +3,15 @@ import styled from 'styled-components';
 export const Title = styled.h1`
     font-size: 80px;
     line-height: 68px;
+    -webkit-text-stroke: 8px var(--light-gray);
+    paint-order: stroke fill;
 `;
 
 export const Subtitle = styled.h2`
     font-size: 40px;
     font-weight: 400;
+    -webkit-text-stroke: 8px var(--light-gray);
+    paint-order: stroke fill;
 `;
 
 export const TextContainer = styled.div`
