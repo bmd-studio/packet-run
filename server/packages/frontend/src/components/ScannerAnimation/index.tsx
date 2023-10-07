f41aff3130269ad39c9489648b9b06ffbe653d95
Lei Nelissen <lei@codified.nl>
2023-10-07 22:16:22 +0200

    fix: scanner animation transitions

--- server/packages/frontend/src/components/ScannerAnimation/index.tsx
+++ server/packages/frontend/src/components/ScannerAnimation/index.tsx
@@ -14,57 +14,82 @@ const ringTransition: RingProps['transition'] = {
     ease: [(val) => val > 0.5 ? 1 : 0, 'easeInOut', 'easeInOut'],
 };
 
-const ringAnimate: RingProps['animate'] = {
-    stroke: ['#000', '#F0EA00', '#000', '#000'],
-};
-
 interface ScannerAnimationProps {
     variant: 'empty' | 'scanning' | 'scanned';
 }
 
+const ringVariants: SVGMotionProps<SVGEllipseElement>['variants'] = {
+    empty: {
+        stroke: ['#000', '#F0EA00', '#000', '#000'],
+    },
+    scanning: {
+        stroke: ['#000']
+    },
+    scanned: {
+        stroke: ['#000']
+    },
+};
+
+const ballVariants: SVGMotionProps<SVGEllipseElement>['variants'] = {
+    empty: {
+        y: [-25, 25, -25],
+    },
+    scanning: {
+        y: [-5, 5, -5],
+    },
+    scanned: {
+        y: [-5, 5, -5],
+    },
+};
+
 export default function ScannerAnimation({ variant }: ScannerAnimationProps) {
     return (
-        <svg viewBox="0 0 500 500" width="500" height="500">
+        <svg viewBox="0 0 500 435" width="500" height="435">
             <motion.g>
                 <Ring
                     cx="250"
-                    cy="480"
+                    cy="410"
                     rx="25"
                     ry="6.25"
                     initial={{ stroke: '#000' }}
-                    animate={variant === 'empty' ? ringAnimate : { stroke: '#000'}}
+                    variants={ringVariants}
+                    animate={variant}
                     transition={{ ...ringTransition, delay: 0 }}
                 />
                 <Ring
                     cx="250"
-                    cy="460"
+                    cy="390"
                     rx="100"
                     ry="25"
-                    animate={variant === 'empty' ? ringAnimate : { stroke: '#000'}}
+                    animate={variant}
+                    variants={ringVariants}
                     transition={{ ...ringTransition, delay: 0.5 }}
                 />
                 <Ring
                     cx="250"
-                    cy="435"
+                    cy="365"
                     rx="175"
                     ry="43.75"
                     initial={{ stroke: '#000' }}
-                    animate={variant === 'empty' ? ringAnimate : { stroke: '#000'}}
+                    animate={variant}
+                    variants={ringVariants}
                     transition={{ ...ringTransition, delay: 1 }}
                 />
                 <Ring
                     cx="250"
-                    cy="400"
+                    cy="330"
                     rx="200"
                     ry="50"
                     initial={{ stroke: '#000' }}
-                    animate={variant === 'empty' ? ringAnimate : { stroke: '#000'}}
+                    animate={variant}
+                    variants={ringVariants}
                     transition={{ ...ringTransition, delay: 1.5 }}
                 />
             </motion.g>
             <motion.g 
                 transition={{ duration: 2, repeat: Infinity, ease: 'easeInOut' }}
-                animate={variant === 'empty' ? { y: [-25, 25, -25] } : { y: 0 }}
+                variants={ballVariants}
+                animate={variant}
             >
                 <ellipse
                     stroke={variant === 'scanned' ? 'var(--yellow)' : 'black'}
@@ -74,10 +99,10 @@ export default function ScannerAnimation({ variant }: ScannerAnimationProps) {
                     rx={150}
                     ry={150}
                     cx={250}
-                    cy={250} 
+                    cy={180} 
                 />
                 <path
-                    d="M100 252C163 292.5 332.5 295 400 252"
+                    d="M100 182C163 222.5 332.5 225 400 182"
                     stroke={variant === 'scanned' ? 'var(--yellow)' : 'black'}
                     fill="none"
                     strokeWidth="8"
