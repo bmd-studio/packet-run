29792c627d97d148ddd360efe1d334fe4ad56464
Lei Nelissen <lei@codified.nl>
2023-10-07 20:28:41 +0200

    fix: add backup fill for titles

--- server/packages/frontend/src/components/Typography/index.tsx
+++ server/packages/frontend/src/components/Typography/index.tsx
@@ -5,6 +5,7 @@ export const Title = styled.h1`
     line-height: 68px;
     -webkit-text-stroke: 8px var(--light-gray);
     paint-order: stroke fill;
+    color: black;
 `;
 
 export const Subtitle = styled.h2`
@@ -12,6 +13,7 @@ export const Subtitle = styled.h2`
     font-weight: 400;
     -webkit-text-stroke: 8px var(--light-gray);
     paint-order: stroke fill;
+    color: black;
 `;
 
 export const TextContainer = styled.div`
