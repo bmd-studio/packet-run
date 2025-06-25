c30a92451a728de352da379325866ffc0e704bf0
Jort <jort@moeilijkedingen.nl>
2025-06-25 18:56:20 +0200

    fix: added filtering for unknown coordinates. So on the map it does not end up in the middle of the ocean.

--- server/packages/frontend/src/components/Map/index.tsx
+++ server/packages/frontend/src/components/Map/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+import { RefObject, useEffect, useRef } from 'react';
 import { useTerminal } from '../RegisterTerminal';
 import mapboxgl, { Map as MapboxMap } from 'mapbox-gl';
 import { MAPBOX_TOKEN } from '@/config';
@@ -10,6 +10,7 @@ import generateDot from './dot';
 import curvedLine from './curvedLine';
 import retrieveLatestKnownHop from '@/lib/latestKnownHop';
 import UnknownMap from './unkown';
+import { filterOutHopsWithUnkownLocation } from '@/lib/hopHelpers';
 
 const MapContainer = styled.div`
     width: 100%;
@@ -26,6 +27,108 @@ export interface MapProps {
         bottom?: number;
     }
 }
+
+function addMarkers(
+    map: RefObject<MapboxMap | null>,
+    markers: [number, number][],
+    currentLocation?: { latitude: number, longitude: number }
+) {
+    if (!map) {
+        return;
+    }
+    const lng = currentLocation?.longitude || -1;
+
+    markers.forEach((coord) => {
+        if (coord[0] === 0 && coord[1] === 0) {
+            return;
+        }
+        if (coord[0] === lng) {
+            // Only add the layer if it doesn't exist yet
+            // TODO: Add the new marker in here
+            if (!map.current?.getLayer('layer-with-orange-dot')) {
+                map.current?.addLayer({
+                    id: 'layer-with-orange-dot',
+                    type: 'symbol',
+                    source: {
+                        type: 'geojson',
+                        data: {
+                            type: 'FeatureCollection',
+                            features: [{
+                                type: 'Feature',
+                                geometry: {
+                                    type: 'Point',
+                                    coordinates: coord,
+                                },
+                                properties: {},
+                            }]
+                        }
+                    },
+                    layout: {
+                        "icon-image": 'orange-dot',
+                    }
+                });
+            }
+        } else {
+            // TODO: add the square marker in here
+            new mapboxgl.Marker({
+                color: coord[0] === lng ? 'var(--orange)' : '#FF0000',
+                scale: 1.0,
+            }).setLngLat(coord)
+                .addTo(map.current as mapboxgl.Map);
+        }
+    });
+}
+
+function addLinesToPreviousHops(
+    map: RefObject<MapboxMap | null>,
+    hops: RegisterTerminalRunHopFragment[],
+) {
+
+    hops.reduce<RegisterTerminalRunHopFragment | null>((prev, hop) => {
+        if (prev && hop.address && prev.address) {
+            map.current?.addLayer({
+                type: 'line',
+                id: `line-${hop.hop}`,
+                source: {
+                    type: 'geojson',
+                    data: curvedLine(runHopToCoords(hop), runHopToCoords(prev)),
+                },
+                paint: {
+                    "line-color": '#FF781F',
+                    "line-width": 3,
+                }
+            });
+        }
+
+        return hop;
+    }, null);
+}
+function addLinesToAvailableHops(
+    map: RefObject<MapboxMap | null>,
+    hops: RegisterTerminalRunHopFragment[],
+    latestKnownHop: RegisterTerminalRunHopFragment | undefined
+) {
+    const address = latestKnownHop?.address;
+    hops.forEach((hop) => {
+        if (latestKnownHop && address && hop.address && hop.address.info?.location?.longitude) {
+            map.current?.addLayer({
+                type: 'line',
+                id: `line-${hop.id}-foreground`,
+                source: {
+                    type: 'geojson',
+                    data: curvedLine(runHopToCoords(hop), runHopToCoords(latestKnownHop)),
+                },
+                paint: {
+                    "line-color": '#FF781F',
+                    "line-width": 3,
+                    'line-dasharray': [4, 4],
+                }
+            });
+        }
+    });
+
+}
+
 export default function Map(props: MapProps) {
     const { shouldDisplayMap, padding = {} } = props;
     const terminal = useTerminal();
@@ -46,6 +149,7 @@ export default function Map(props: MapProps) {
         const lat = address?.info?.location?.latitude;
 
         if (!lng || !lat) return;
+        const currentLocation = { longitude: lng, latitude: lat };
 
         // Determine where the center of the map should be
         const center: [number, number] = [lng, lat + 0.15];
@@ -70,14 +174,16 @@ export default function Map(props: MapProps) {
         const orangeDot = generateDot();
         map.current.addImage('orange-dot', orangeDot)
 
-        const previousRoutes = run.hops.filter((h) => (
+        const previousRoutes = filterOutHopsWithUnkownLocation(run.hops).filter((h) => (
             h.address && h.status === RunHopStatus.Actual
         )).sort((a, b) => a.hop - b.hop);
 
-        const markers = (terminal.type === TerminalType.Receiver
+        // create coordinates for all hops and filters out hops with unkown locations
+        const markers = filterOutHopsWithUnkownLocation((terminal.type === TerminalType.Receiver
             ? [...previousRoutes, ...run.availableHops]
             : [...(latestKnownHop ? [latestKnownHop] : []), ...run.availableHops]
-        ).map(runHopToCoords);
+        ))
+            .map(runHopToCoords);
 
         // Then, create a new bound from all coordinates
         const bounds = markers.reduce((bounds, coord) => {
@@ -95,81 +201,13 @@ export default function Map(props: MapProps) {
 
         map.current.on('load', () => {
             // Create a marker for each coordinate
-            markers.forEach((coord) => {
-                if (coord[0] === lng) {
-                    // Only add the layer if it doesn't exist yet
-                    // TODO: Add the new marker in here
-                    if (!map.current?.getLayer('layer-with-orange-dot')) {
-                        map.current?.addLayer({
-                            id: 'layer-with-orange-dot',
-                            type: 'symbol',
-                            source: {
-                                type: 'geojson',
-                                data: {
-                                    type: 'FeatureCollection',
-                                    features: [{
-                                        type: 'Feature',
-                                        geometry: {
-                                            type: 'Point',
-                                            coordinates: coord,
-                                        },
-                                        properties: {},
-                                    }]
-                                }
-                            },
-                            layout: {
-                                "icon-image": 'orange-dot',
-                            }
-                        });
-                    }
-                } else {
-                    // TODO: add the square marker in here
-                    new mapboxgl.Marker({
-                        color: coord[0] === lng ? 'var(--orange)' : '#FF0000',
-                        scale: 1.0,
-                    }).setLngLat(coord)
-                        .addTo(map.current as mapboxgl.Map);
-                }
-            });
-
-            previousRoutes.reduce<RegisterTerminalRunHopFragment | null>((prev, hop) => {
-                if (prev && hop.address && prev.address) {
-                    map.current?.addLayer({
-                        type: 'line',
-                        id: `line-${hop.hop}`,
-                        source: {
-                            type: 'geojson',
-                            data: curvedLine(runHopToCoords(hop), runHopToCoords(prev)),
-                        },
-                        paint: {
-                            "line-color": '#FF781F',
-                            "line-width": 3,
-                        }
-                    });
-                }
-
-                return hop;
-            }, null);
-
-            run.availableHops.forEach((hop) => {
-                if (latestKnownHop && address && hop.address && hop.address.info?.location?.longitude) {
-                    map.current?.addLayer({
-                        type: 'line',
-                        id: `line-${hop.id}-foreground`,
-                        source: {
-                            type: 'geojson',
-                            data: curvedLine(runHopToCoords(hop), runHopToCoords(latestKnownHop)),
-                        },
-                        paint: {
-                            "line-color": '#FF781F',
-                            "line-width": 3,
-                            'line-dasharray': [4, 4],
-                        }
-                    });
-                }
-            });
+            addMarkers(map, markers, currentLocation);
+            // add all the lines to previous hops
+            addLinesToPreviousHops(map, previousRoutes);
+            // TODO:: should add markers.
+            // add lines to available hops
+            addLinesToAvailableHops(map, filterOutHopsWithUnkownLocation(run.availableHops), latestKnownHop);
         });
-
         return () => map.current?.remove();
     }, [run, terminal.type, shouldDisplayMap, padding]);
 
