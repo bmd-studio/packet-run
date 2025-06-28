71e13b8467b7a65018eccf5817767f49f71f4625
Lei Nelissen <lei@codified.nl>
2025-06-28 08:19:25 +0200

    feat: include buildtime in image name

--- image/build.sh
+++ image/build.sh
@@ -18,6 +18,9 @@ fi
 # Get git commit SHA for versioning the output files
 GIT_SHA=$(git rev-parse --short HEAD)
 
+# Get current date in ISO format for sorting
+BUILD_DATE=$(date +%Y%m%d%H%M%S)
+
 # Run the build using a heredoc for better readability
 podman compose run --rm build-image sh -c '
     # Exit on any error
@@ -33,12 +36,12 @@ podman compose run --rm build-image sh -c '
     mv /home/imagegen/rpi-image-gen/work/deb12-arm64-min/artefacts/deb12-arm64-min.* ./deploy/
 '
 
-# Rename the output files to include type and git SHA
+# Rename the output files to include type, date, and git SHA
 for file in deploy/deb12-arm64-min.*; do
     if [ -f "$file" ]; then
         # Extract the file extension
         extension="${file##*.}"
         # Rename the file with the new format
-        mv "$file" "output/packet-run-img-$1-$GIT_SHA.$extension"
+        mv "$file" "output/packet-run-img-$1-$BUILD_DATE-$GIT_SHA.$extension"
     fi
 done 
